020d80000e353a2969bac40a262f3e6a06fd2602
p1mps <andrea@shellrent.com>
2017-12-10 20:21:37 +0100

    Add Welcome Login Register views

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,24 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+	return view('welcome');
+})->name('welcome');
+
+Route::get('/rules', function () {
+	return view('rules');
+})->name('rules');
+
+Route::get('/match', function () {
+	return view('match');
+})->name('match');
+
+Route::get('/rank', function () {
+	return view('rank');
+})->name('rank');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
